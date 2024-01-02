8aa95905909be1d9c25136647b46f13b8da046f8
Janrey Licas <janjan.licas@gmail.com>
2024-01-02 19:04:57 +0800

    chore(example): code `format`

--- examples/7_price_scale_options/price_scale_options.py
+++ examples/7_price_scale_options/price_scale_options.py
@@ -3,10 +3,10 @@ from lightweight_charts import Chart
 
 reset_price_scale = lambda chart: chart.price_scale(auto_scale=True)
 invert_price_scale = lambda chart: chart.price_scale(invert_scale=True)
-mode_idx_to_100 = lambda chart: chart.price_scale(mode='index100')
+mode_idx_to_100 = lambda chart: chart.price_scale(mode="index100")
 visible_off = lambda chart: chart.price_scale(visible=False)
-disable_aligned_label = lambda chart: chart.price_scale(align_labels = False)
-min_width_to_100 = lambda chart: chart.price_scale(minimum_width = 100)
+disable_aligned_label = lambda chart: chart.price_scale(align_labels=False)
+min_width_to_100 = lambda chart: chart.price_scale(minimum_width=100)
 
 
 if __name__ == "__main__":
@@ -17,7 +17,9 @@ if __name__ == "__main__":
 
     chart.topbar.button("Auto", "Auto", func=reset_price_scale)
     chart.topbar.button("Invert", "Invert", func=invert_price_scale)
-    chart.topbar.button("Mode to Indexed 100", "Mode to Indexed 100", func=mode_idx_to_100)
+    chart.topbar.button(
+        "Mode to Indexed 100", "Mode to Indexed 100", func=mode_idx_to_100
+    )
     chart.topbar.button("Hide Price Scale", "Price Scale", func=visible_off)
     chart.topbar.button("Disable Align", "Disable Align", func=disable_aligned_label)
     chart.topbar.button("Min Width to 100", "Min Width to 100", func=min_width_to_100)
