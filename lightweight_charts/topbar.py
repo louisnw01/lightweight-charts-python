cdfdc8bf464d63bb21cc64df9d04a62c273af7a8
DIMITRIOS CHRYSOCHERIS <jimchr2001@gmail.com>
2024-09-27 22:49:20 +0300

    Added button right click action

--- lightweight_charts/topbar.py
+++ lightweight_charts/topbar.py
@@ -8,30 +8,40 @@ ALIGN = Literal['left', 'right']
 
 
 class Widget(Pane):
-    def __init__(self, topbar, value, func: callable = None, convert_boolean=False):
+    def __init__(self, topbar, value, func: callable = None, right_click_func: callable = None, font_size: str = '16px', convert_boolean=False):
         super().__init__(topbar.win)
         self.value = value
 
+        if func and not callable(func):
+            raise TypeError(f"The provided 'func' is not callable: {func}")
+        
+        # Left-click wrapper
         def wrapper(v):
             if convert_boolean:
                 self.value = False if v == 'false' else True
             else:
                 self.value = v
-            func(topbar._chart)
-
+            if func:
+                func(topbar._chart)
+        
+        # Right-click wrapper
+        def right_click_wrapper(v):
+            if right_click_func:
+                right_click_func(topbar._chart)
+        
         async def async_wrapper(v):
             self.value = v
             await func(topbar._chart)
-
+        
         self.win.handlers[self.id] = async_wrapper if asyncio.iscoroutinefunction(func) else wrapper
+        if right_click_func:
+            self.win.handlers[self.id + '_right_click'] = right_click_wrapper
 
 
 class TextWidget(Widget):
     def __init__(self, topbar, initial_text, align, func):
         super().__init__(topbar, value=initial_text, func=func)
-
         callback_name = f'"{self.id}"' if func else ''
-
         self.run_script(f'{self.id} = {topbar.id}.makeTextBoxWidget("{initial_text}", "{align}", {callback_name})')
 
     def set(self, string):
@@ -76,28 +86,47 @@ class MenuWidget(Widget):
 
 
 class ButtonWidget(Widget):
-    def __init__(self, topbar, button, separator, align, toggle, disabled: bool = False, font_size: str = '16px', func=None):
+    def __init__(self, topbar, button, separator, align, toggle, disabled: bool = False, func=None, right_click_func=None, font_size: str = '16px'):
         super().__init__(topbar, value=False, func=func, convert_boolean=toggle)
         self.disabled = disabled
         self.font_size = font_size
+        self.right_click_func = right_click_func
+
         self.run_script(
             f'{self.id} = {topbar.id}.makeButton("{button}", "{self.id}", {jbool(separator)}, true, "{align}", {jbool(toggle)})'
         )
         self.update_disabled()
+        if right_click_func:
+            self.enable_right_click()
 
     def set(self, string):
         self.run_script(f'{self.id}.elem.innerText = "{string}"')
 
     def update_disabled(self):
         """Update the button's disabled state and text opacity in the UI."""
-        # Generate a unique name for the button element in JavaScript
-        unique_button_elem = f'buttonElem_{self.id.replace(".", "_")}'  # Replace '.' to avoid issues in variable naming
+        unique_button_elem = f'buttonElem_{self.id.replace(".", "_")}'  # Unique reference for each button
         self.run_script(f'''
-            const {unique_button_elem} = {self.id}.elem;  // Unique reference for each button
+            const {unique_button_elem} = {self.id}.elem;
             {unique_button_elem}.disabled = {jbool(self.disabled)};
             {unique_button_elem}.style.opacity = {0.5 if self.disabled else 1};
-            {unique_button_elem}.style.fontSize = "{self.font_size}";  // Set the font size
+            {unique_button_elem}.style.fontSize = "{self.font_size}";
         ''')
+
+    def enable_right_click(self):
+        """Enable right-click functionality for the button."""
+        unique_button_elem = f'buttonElem_{self.id.replace(".", "_right_")}'  # Unique reference for each button
+
+        self.run_script(f'''
+            const {unique_button_elem} = {self.id}.elem;
+            {unique_button_elem}.addEventListener('contextmenu', (event) => {{
+                event.preventDefault();  // Prevent the default right-click context menu
+                if ({unique_button_elem}.disabled) return;  // Ignore right-click if button is disabled
+                window.callbackFunction(`right_click_{self.id}_~_` + event.button);
+            }});
+        ''')
+
+        # Set up a handler for the right-click event
+        self.win.handlers[f'right_click_{self.id}'] = self.right_click_func
 
     def disable(self):
         """Disable the button."""
@@ -147,6 +176,7 @@ class TopBar(Pane):
         self._widgets[name] = TextWidget(self, initial_text, align, func)
 
     def button(self, name, button_text: str, separator: bool = True,
-             align: ALIGN = 'left', toggle: bool = False, disabled: bool = False, font_size: str = '16px', func: callable = None):
+           align: ALIGN = 'left', toggle: bool = False, disabled: bool = False, 
+           font_size: str = '16px', func: callable = None, right_click_func: callable = None):
         self._create()
-        self._widgets[name] = ButtonWidget(self, button_text, separator, align, toggle, disabled, font_size, func)
+        self._widgets[name] = ButtonWidget(self, button_text, separator, align, toggle, disabled, func, right_click_func, font_size)
